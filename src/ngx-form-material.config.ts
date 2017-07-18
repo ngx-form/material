370bd93096a13852e47dfd8a195277d4c953fb88
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-18 17:46:39 +0200

    refactor: move config to separate file

---
+++ src/ngx-form-material.config.ts
@@ -0,0 +1,18 @@
+import {
+  FormMaterialInputComponent
+} from './element';
+
+import {
+  FormElementConfig
+} from '@ngx-form/element';
+
+export const elementsConfig: FormElementConfig = {
+  elements: [
+    { name: 'input', component: FormMaterialInputComponent }
+  ],
+  errorMessages: {
+    required: {
+      default: 'This field is required'
+    }
+  }
+}
