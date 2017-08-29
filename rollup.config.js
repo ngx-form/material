d607dc74a629e7317b7a50da21f2229c653c5546
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-29 14:34:59 +0200

    chore(rollup.config): use config from rollup-angular-package

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,63 @@
 
+import angular from 'rollup-plugin-angular';
+import buble from 'rollup-plugin-buble';
 import commonjs from 'rollup-plugin-commonjs';
-import html from 'rollup-plugin-html';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
+
+// rollup-plugin-angular addons
+import sass from 'node-sass';
+import CleanCSS from 'clean-css';
+import { minify as minifyHtml } from 'html-minifier';
+
+const cssmin = new CleanCSS();
+const htmlminOpts = {
+  caseSensitive: true,
+  collapseWhitespace: true,
+  removeComments: true,
+};
 
 export default {
-  entry: 'dist/index.js',
-  dest: 'dist/bundle.umd.js',
-  exports: 'named',
-  sourceMap: false,
-  format: 'umd',
-  moduleName: '@ngx-form/material',
+  input: 'dist/index.js',
+  output: {
+    // core output options
+    file: 'dist/bundle.umd.js',    // required
+    format: 'umd',  // required
+    name: 'ngx-form.material',
+
+    // advanced output options
+    // paths: ,
+    // banner: ,
+    // footer: ,
+    // intro:,
+    // outro: ,
+    sourcemap: true, // true | inline
+    // sourcemapFile: ,
+    // interop: ,
+
+    // danger zone
+    exports: 'named',
+    // amd: ,
+    // indent: , 
+    // strict: 
+  },
   onwarn,
   plugins: [
-    commonjs({
-      namedExports: {
-        'node_modules/rxjs/**': ['named']
+    angular({
+      preprocessors: {
+        template: template => minifyHtml(template, htmlminOpts),
+        style: scss => {
+          const css = sass.renderSync({ data: scss }).css;
+          return cssmin.minify(css).styles;
+        },
       }
     }),
-    html({
-      include: [
-        '../**/*.html',
-        '**/*.html'
-      ],
-      htmlMinifierOptions: {
-        caseSensitive: true // need to do not lower letter
-      }
-    }),
+    buble(),
+    commonjs(),
     nodeResolve({
-      // use "es2015" field for ES2015 modules with ES2015 code,
-	    // if possible
-      es2015: true, // Default: false
-
-      // use "module" field for ES2015 modules with ES5 code,
-      // if possible
+      // use "module" field for ES6 module if possible
       module: true, // Default: true
 
       // use "jsnext:main" if possible
@@ -46,19 +69,36 @@ export default {
       // – see https://github.com/rollup/rollup-plugin-commonjs
       main: true,  // Default: true
 
-      extensions: [ '.js', '.json', '.html']
+      // some package.json files have a `browser` field which
+      // specifies alternative files to load for people bundling
+      // for the browser. If that's you, use this option, otherwise
+      // pkg.browser will be ignored
+      browser: true,  // Default: false
+
+      // not all files you want to resolve are .js files
+      extensions: [ '.js', '.json' ],  // Default: ['.js']
+
+      // whether to prefer built-in modules (e.g. `fs`, `path`) or
+      // local ones with the same names
+      preferBuiltins: true,  // Default: true
+
+      // Lock the module search in this path (like a chroot). Module defined
+      // outside this path will be mark has external
+      jail: '/', // Default: '/'
+
+      // If true, inspect resolved files to check that they are
+      // ES2015 modules
+      modulesOnly: false, // Default: false
+
+      // Any additional options that should be passed through
+      // to node-resolve
+      customResolveOptions: {}
     }),
     typescript({
       typescript: require('./node_modules/typescript')
     }),
-    uglify()
-  ],
-  globals: {
-    '@angular/animations': 'ng.animations',
-    '@angular/core': 'ng.core',
-    '@angular/http': 'ng.http',
-    '@angular/material': 'ng.material'
-  }
+    uglify({}, minify)
+  ]
 };
 
 function onwarn(message) {
